5526a1c7adbe60500ee0f1f4578b9f49c0398eb9
shabil <shabilnptvr@gmail.com>
2023-08-13 12:32:41 +0530

    my first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+var video = document.getElementById('myvideo')
+var btn = document.getElementById('mybtn')
+function myFunction() {
+    if (video.paused) {
+      video.play();
+      btn.innerHTML = "Pause";
+    } else {
+      video.pause();
+      btn.innerHTML = "Play";
+    }
+  }
